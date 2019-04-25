118ab80fab9fd7d535d27f9e956c98b80e059119
Mevericks <reloved@gmail.com>
2019-04-25 20:44:45 +0300

    вывод Auth::user()

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,10 +6,17 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class AdminController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function show(){
+        $user = Auth::user();
+        dump($user);
         return view('welcome');
     }
 }
