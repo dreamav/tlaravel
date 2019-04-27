e621a7be278eeab3f9e14475dbf9177481b33c78
Mevericks <reloved@gmail.com>
2019-04-27 19:25:17 +0300

    использование проверки правила update-article в контроллере

--- app/Http/Controllers/Admin/AdminUpdatePostController.php
+++ app/Http/Controllers/Admin/AdminUpdatePostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Gate;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,37 +10,42 @@ use App\Http\Controllers\Controller;
 
 use App\Article;
 use Auth;
+use Session;
 
 class AdminUpdatePostController extends Controller
 {
     //show Form
     public function show(Request $request, $id) {
-
     	$article = Article::find($id);
 		return view('default.update_post',['title' => 'Редактирование материала','article' => $article]);
 	}
 	
 	//new post
     public function create(Request $request) {
-    	
-    	$this->validate($request,[
-    		'name'=>'required'
-    	]);
-    	
-    	$user = Auth::user();
+
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+
+        $user = Auth::user();
 
         $data = $request->except('_token');
 
         $article = Article::find($data['id']);
 
-        $article->name = $data['name'];
-        $article->img = $data['img'];
-        $article->text = $data['text'];
+        if (Gate::allows('update-article', $article)) {
+            $article->name = $data['name'];
+            $article->img = $data['img'];
+            $article->text = $data['text'];
 
-        $res = $user->articles()->save($article);
+            $res = $user->articles()->save($article);
 
-        return redirect()->back()->with('message', 'Материал обновлен');
+            Session::remove('message');
 
+            return redirect()->back()->with('message', 'Материал обновлен');
+        }
+        Session::put(['message' => 'У вас нет прав']);
+        return redirect()->back();
 
     }
 }
